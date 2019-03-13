9e721c090dc13510facb9ce1ac82bd4fc9052dac
Sabrina Wong <swong040@ucr.edu>
2019-03-13 10:08:30 -0700

    parts of parts done

--- twitter_streaming.py
+++ twitter_streaming.py
@@ -12,7 +12,14 @@
 # 	handle fields like username, location, etc.
 # 	test for retrieval of relevant documents given a query
 
-# STREAMING TO GET TWEETS
+# part 3 extension
+# 	web-based interface
+# 	- contain a textbox and search button
+# 	- list of results (first 10) and their scores > decreasing order
+# 	- twitter:
+#		order by combination of time and relevance
+#		describe ranking function
+# 	use web dev of choice
 
 #Import the necessary methods from tweepy library
 from BeautifulSoup import BeautifulSoup
@@ -23,6 +30,10 @@ from elasticsearch import Elasticsearch
 import apiKeys		#file for apikey
 import os, re
 import json, requests, urllib2
+
+from datetime import datetime
+from flask import Flask, jsonify, request
+
  
 
 #Variables that contains the user credentials to access Twitter API
@@ -32,10 +43,33 @@ consumer_key = apiKeys.consumer_key
 consumer_secret = apiKeys.consumer_secret
 
 filename = 'fetched_tweets.json'
+filename1 = 'tweet.json'
 
 res = requests.get('http://localhost:9200')
 # print(res.content)
-es = Elasticsearch()
+es = Elasticsearch('http://localhost:9200')
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+	# results = es.get(index='twitter', doc_type='tweet',id='')
+	return '<h1>Welcome!</h1>'
+
+# fix
+@app.route('/search', methods=['POST'])
+def search():
+	keyword = request.form['keyword']
+
+	body = {
+		"query": {
+			"multi_match": {
+				"query": keyword,
+				"fields": ["screen_name", "text", "timestamp"]
+			}
+		}
+	}
+	res = es.search(index='twitter', doc_type='tweet', body=body)
+	return jsonify(res['hits']['hits'])
 
 
 # looks for URL in tweet content
@@ -64,10 +98,12 @@ def GetUrlTitles(string):
 class StdOutListener(StreamListener):
 	def on_data(self, data):
 		if os.path.getsize(filename) < 100000: #1,000,000
+			with open(filename1, 'w+') as t:
+				t.write(data)
+				tweet = json.loads(data)
+				parseToES(tweet)
 			tf.write(data)
 			print(os.path.getsize(filename))
-
-
 			return True
 		return False
 
@@ -75,31 +111,30 @@ class StdOutListener(StreamListener):
 		print status
 
 # load json into elasticsearch
-def parseToES():
-	es.indices.delete(index='twitter', ignore=[400, 404])
-	with open(filename, 'r') as tf:
-		for line in tf:
-			tweet = json.loads(line)
-			try:
-				dic = {
-					'tweet_id': tweet['id'],
-					'screen_name': tweet['user']['screen_name'],
-					'text': tweet['text'],
-					'timestamp': tweet['created_at'],
-					'location': tweet['place']['full_name']
+def parseToES(string):
 
-				}
-				if FindHashtag(tweet['text']):
-					dic['hashtags'] = FindHashtag(tweet['text'])
-				if FindURL(tweet['text']):
-					dic['urlTitles'] = GetUrlTitles(tweet['text'])
-				es.index(index = "twitter", doc_type = 'tweet', body = dic )
-				print(json.dumps(dic, ensure_ascii = False))
-			except:
-				import traceback
-				traceback.print_exc()
-				pass
+	# json.load(string, tweet)
+	try:
+		dic = {
+			'tweet_id': string['id'],
+			'screen_name': string['user']['screen_name'],
+			'text': string['text'],
+			'timestamp': string['created_at'],
+			'location': string['place']['full_name']
 
+		}
+		if FindHashtag(string['text']):
+			dic['hashtags'] = FindHashtag(string['text'])
+		if FindURL(string['text']):
+			dic['urlTitles'] = GetUrlTitles(string['text'])
+		es.index(index = "twitter", doc_type = 'tweet', body = dic )
+		print(json.dumps(dic, ensure_ascii = False))
+	except:
+		import traceback
+		traceback.print_exc()
+		pass
+
+# def GetQuery(string):
 
 
 if __name__ == '__main__':
@@ -109,9 +144,20 @@ if __name__ == '__main__':
 	auth.set_access_token(access_token, access_token_secret)
 	stream = Stream(auth, l)
 
+	if filename and filename1 and es.indices.exists(index = 'twitter'):
+		os.remove(filename)		# big json file
+		print("File removed")
+		os.remove(filename1)	# indivisual json to index
+		print("File1 removed")
+		es.indices.delete(index='twitter', ignore=[400, 404])
+		print("Index deleted")
+
 	#filter Twitter Streams to capture data by location (san francisco) 
 	with open(filename, "a+") as tf:
 		stream.filter(locations = [-122.75,36.8,-121.75,37.8])
 
-	parseToES()
 	stream.disconnect()
+
+	# parseToES()
+
+app.run(port=5000, debug=True)
